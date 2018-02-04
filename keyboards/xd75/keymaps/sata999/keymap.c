d6bfd9d8f306f91b2c77954f98d861cb508205a1
SaTa999 <s.takanashi0120@gmail.com>
2018-02-04 22:34:48 +0900

    change layout

--- keyboards/xd75/keymaps/sata999/keymap.c
+++ keyboards/xd75/keymaps/sata999/keymap.c
@@ -26,71 +26,71 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* QWERTY
  * .--------------------------------------------------------------------------------------------------------------------------------------.
- * | ESC    | 1      | 2      | 3      | 4      | 5      | -      | `      | =      | 6      | 7      | 8      | 9      | 0      | BACKSP |
+ * | ESC    | 1      | 2      | 3      | 4      | 5      | NUM LK | HOME   | PG UP  | 6      | 7      | 8      | 9      | 0      | DEL    |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
- * | TAB    | Q      | W      | E      | R      | T      | [      | \      | ]      | Y      | U      | I      | O      | P      | '      |
+ * | TAB    | Q      | W      | E      | R      | T      | PR SCR | END    | PG DN  | Y      | U      | I      | O      | P      | BACKSP |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
- * | LCTRL  | A      | S      | D      | F      | G      | HOME   | DEL    | PG UP  | H      | J      | K      | L      | ;      | ENTER  |
+ * | LCTRL  | A      | S      | D      | F      | G      | -      | =      | \      | H      | J      | K      | L      | ;      | ENTER  |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
- * | LSHIFT | Z      | X      | C      | V      | B      | END    | UP     | PG DN  | N      | M      | ,      | .      | /      | RSHIFT |
+ * | LSHIFT | Z      | X      | C      | V      | B      | [      | `      | ]      | N      | M      | ,      | .      | /      | '      |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
- * | LWIN   | LALT   | FN     | HENKAN | SPACE  | SPACE  | LEFT   | DOWN   | RIGHT  | SPACE  | SPACE  | MHEN   | FN     | RALT   | RWIN   |
+ * | HANKAKU| LALT   | LWIN   | FN     | MHEN   | SPACE  | SPACE  | FN     | SPACE  | SPACE  | HENKAN | LEFT   | DOWN   | UP     | RIGHT  |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
     [_QW] = {
         /* QWERTY */
-        {KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_MINS, KC_GRV, KC_EQL, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC},
-        {KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_LBRC, KC_BSLS, KC_RBRC, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_QUOT},
-        {KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_HOME, KC_DEL, KC_PGUP, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT},
-        {KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_END, KC_UP, KC_PGDN, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT},
-        {KC_LGUI, KC_LALT, MO(_FN), KC_HENK, KC_SPC, LT(MO(_FN), KC_SPC), KC_LEFT, KC_DOWN, KC_RGHT, LT(MO(_FN), KC_SPC), KC_SPC, KC_MHEN, MO(_MS), KC_RALT, KC_RCTL},
+        {KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_NLCK, KC_HOME, KC_PGUP, KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS},
+        {KC_TRNS, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_QUOT, KC_END, KC_PGDN, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_TRNS},
+        {KC_TRNS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_MINS, KC_EQL, KC_BSLS, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_TRNS},
+        {KC_TRNS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LBRC, KC_GRV, KC_RBRC, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_TRNS},
+        {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS},
     },
 
     /* FUNCTION
  * .--------------------------------------------------------------------------------------------------------------------------------------.
- * | HANKAKU| F1     | F2     | F3     | F4     | F5     | F11    | F12    | NUM LK | F6     | F7     | F8     | F9     | F10    | DEL    |
+ * |        | F1     | F2     | F3     | F4     | F5     |        |        |        | F6     | F7     | F8     | F9     | F10    | F11    |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | SELECT | P1     | P2     | P3     | P4     | P5     |        |        |        | P6     | P7     | P8     | P9     | P0     | PR SCR |
+ * |        | P1     | P2     | P3     | P4     | P5     |        |        |        | P6     | P7     | P8     | P9     | P0     | F12    |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |        |        |        |        | MINS   |        |        |        |        |        |        |        |        |        |        |
+ * |        |        |        |        |        |        |        |        |        | LEFT   | DOWN   | UP     | RIGHT  |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | RESET  |        |        | FN     |        |        |        |        |        |        |        | FN     |        |        |        |
+ * | RESET  | C+A+DEL|        | FN     |        |        |        |        |        |        |        |        |        |        |        |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
     [_FN] = {
         /* FUNCTION */
-        {KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F11, KC_F12, KC_NLCK, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_DEL},
-        {KC_MSEL, KC_P1, KC_P2, KC_P3, KC_P4, KC_P5, KC_TRNS, KC_TRNS, KC_TRNS, KC_P6, KC_P7, KC_P8, KC_P9, KC_P0, KC_PSCR},
-        {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINS, KC_TRNS, KC_TRNS, KC_P5, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS},
-        {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS},
-        {RESET, KC_TRNS, MO(_FN), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(_FN), LALT(LCTL(KC_DEL)), KC_TRNS},
+        {KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_TRNS, KC_TRNS, KC_TRNS, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11},
+        {KC_TRNS, KC_P1, KC_P2, KC_P3, KC_P4, KC_P5, KC_TRNS, KC_TRNS, KC_TRNS, KC_P6, KC_P7, KC_P8, KC_P9, KC_P0, KC_F12},
+        {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_TRNS, KC_TRNS},
+        {KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS},
+        {RESET, LCA(KC_DEL), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS},
     },
 
     /* MODSHIFT
  * .--------------------------------------------------------------------------------------------------------------------------------------.
- * | F1     | F2     | F3     | F4     | F5     | F6     | NUM LK | P/     | P*     | F7     | F8     | F9     | F10    | F11    | F12    |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | SELECT | P1     | P2     | P3     | P4     | P5     |        |        |        | P6     | P7     | P8     | P9     | P0     | PR SCR |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | PREV   | PLAY   | NEXT   | STOP   |        |        | P4     | P5     | P6     | +      |        |        |        |        |        |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | VOL-   | MUTE   | VOL+   |        |        |        | P1     | P2     | P3     | PENT   |        |        |        |        |        |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | RESET  |        |        | FN     |        |        | P0     |        | P.     | PENT   | PENT   | FN     |        |        |        |
+ * | ESC    | 1      | 2      | 3      | 4      | 5      | NUM LK | HOME   | PG UP  | 6      | 7      | 8      | 9      | 0      | PR SCR |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
+ * | TAB    | Q      | W      | E      | R      | T      | '      | END    | PG DN  | Y      | U      | I      | O      | P      | DEL    |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
+ * | LCTRL  | A      | S      | D      | F      | G      | -      | +      | \      | H      | J      | K      | L      | ;      | ENTER  |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
+ * | LSHIFT | Z      | X      | C      | V      | B      | [      | `      | ]      | N      | M      | ,      | .      | /      | BSPC   |
+ * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
+ * | HANKAKU| LALT   | LWIN   | FN     | MHEN   | SPACE  | SPACE  | FN     | SPACE  | SPACE  | HENKAN | LEFT   | DOWN   | UP     | RIGHT  |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
     [_MS] = {
         /* MODSHIFT */
-        {KC_ESC, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0},
-        {KC_TAB, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0},
+        {KC_ESC, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_PSCR},
+        {KC_TAB, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_DEL},
         {KC_LCTL, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_ENT},
-        {KC_LSFT, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_RSFT},
-        {KC_LGUI, KC_LALT, MO(_FN), LT(MO(_FN), KC_HENK), KC_SPC, LT(MO(_FN), KC_SPC), KC_LEFT, KC_DOWN, KC_RGHT, LT(MO(_FN), KC_SPC), KC_SPC, LT(MO(_FN), KC_MHEN), MO(_MS), KC_RALT, KC_RCTL},
+        {OSM(MOD_LSFT), KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_FN0, KC_BSPC},
+        {KC_GRV, KC_LALT, KC_LGUI, MO(_FN), KC_MHEN, KC_SPC, LT(_FN, KC_SPC), OSL(_FN), LT(_FN, KC_SPC), KC_SPC, KC_HENK, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT},
     }};
 
 /*
